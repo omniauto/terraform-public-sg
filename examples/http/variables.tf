b8c405b5337cbb2f4492c6e942cc758539bd20a5
csomnirpa <cs@omnirpa.io>
2023-03-11 21:57:32 -0800

    Security Group

---
+++ examples/http/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  type = string
+  description = "The AWS Region"
+  default = "us-west-1"
+}
+
+variable "aws_security_group" {
+  type = string
+  description = "AWS Security Group"
+  default = "omni-security-group"
+}
+
